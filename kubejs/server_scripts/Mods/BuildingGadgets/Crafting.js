f0c8dadbdc91bb72d5d76ad42c185abf23a5ec35
ACrazyD <merlinsky2000@gmail.com>
2024-01-01 16:44:36 -0800

    Sinew Fibers

---
+++ kubejs/server_scripts/Mods/BuildingGadgets/Crafting.js
@@ -0,0 +1,11 @@
+console.info('Milling my mind away')
+
+ServerEvents.recipes(e => {
+    
+    //Silent Gear Sinew Milling
+    e.recipes.create.milling('6x silentgear:sinew_fiber', 'silentgear:dried_sinew')
+    
+    
+
+
+})//End Of File
